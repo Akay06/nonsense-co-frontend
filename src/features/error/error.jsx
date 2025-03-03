5c78d2081e382c9ab5f3a310879f8efea2db682f
Akay06 <aravindraghavan98@gmail.com>
2025-03-03 00:30:13 -0600

    Error page fix

--- src/features/error/error.jsx
+++ src/features/error/error.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./error.module.scss";
+import styles from "./error.module.css";
 import scareCrow from "../../assets/Scarecrow.png";
 
 const ErrorPage = () => {
